9b0c3fa12506cbbd726d60d3f952c78ad9e56992
Peter Schultz <kaedenn@gmail.com>
2018-11-19 16:10:38 -0900

    Add hooks for subscription-like events

--- twitch-events.js
+++ twitch-events.js
@@ -21,7 +21,6 @@ function TwitchClient(opts) {
         _channelNames = {},
         _channelRooms = {};
 
-
     //publics
     this.Debug = opts.Debug || false;
     this.Channels = [];
@@ -41,8 +40,6 @@ function TwitchClient(opts) {
         this.ClientID = _clientId;
     }
 
-    
-
     //websocket handlers
     function OnWebsocketOpen() {
         this.send('CAP REQ :twitch.tv/tags twitch.tv/commands twitch.tv/membership');
@@ -54,8 +51,6 @@ function TwitchClient(opts) {
         else {
             this.send(`NICK justinfan${Math.floor(Math.random() * 999999)}`);
         }
-
-        
 
         var idx=0;
         while (idx < client.PendingChannels.length) {
@@ -97,28 +92,23 @@ function TwitchClient(opts) {
             else if (parts[1] == 'JOIN') {
                 if (client.onJoin)
                     client.onJoin(parts[0].split(':')[1].split('!')[0], parts[2]);
-                //fireEvent('onJoin', line);
             }
             else if (parts[1] == 'PART') {
                 if (client.onPart)
                     client.onPart(parts[0].split(':')[1].split('!')[0], parts[2]);
-                //fireEvent('onPart', line);
             }
             else if (parts[1] == 'MODE') {
-                //fireEvent('onMode', line);
+                if (client.onMode)
+                    client.onMode(line);
             }
             else if (parts[2] == 'PRIVMSG') {
-
 /*
-
 @badges=broadcaster/1;color=#1E90FF;display-name=timeshifter08;emotes=;id=d653a0d0-1cd0-492e-ad6e-2de1940a8d16;mod=0;room-id=61927669;subscriber=0;tmi-sent-ts=1524771643982;turbo=0;user-id=61927669;user-type=
  :timeshifter08!timeshifter08@timeshifter08.tmi.twitch.tv PRIVMSG #timeshifter08 :asdf
 
 @badges=broadcaster/1;color=#1E90FF;display-name=timeshifter08;emotes=;id=847906fc-5db5-4ce5-9258-f206b94d9a31;mod=0;room-id=61927669;subscriber=0;tmi-sent-ts=1524771400844;turbo=0;user-id=61927669;user-type=
  :timeshifter08!timeshifter08@timeshifter08.tmi.twitch.tv PRIVMSG #chatrooms:61927669:a0c1e70d-7d52-491a-bcd5-6db6a5492d64 :asdf
-
 */
-
                 var userdata = parts[0].split(';');
 
                 var userdata_obj = {};
@@ -127,33 +117,25 @@ function TwitchClient(opts) {
                     var sides = s.split('=');
                     userdata_obj[sides[0]] = sides[1];
                 }
-                
 
                 var user = parts[1].split(':')[1].split('!')[0],
                     channel = parts[3];
-                
+
                 var idx = line.indexOf(':', line.indexOf(channel));
                 var msg = line.substring(idx + 1);
-                
 
                 if (client.onPrivmsg)
                     client.onPrivmsg(user, channel.substring(1), msg, userdata_obj, line);
 
             }
             else if (parts[2] == 'ROOMSTATE') {
-
                 /*
-
 @broadcaster-lang=;emote-only=0;followers-only=-1;r9k=0;rituals=1;room-id=61927669;slow=0;subs-only=0 :tmi.twitch.tv ROOMSTATE #timeshifter08
-
 
 @badges=broadcaster/1;color=#1E90FF;display-name=timeshifter08;emotes=;id=377f9462-5f5e-4e5a-a3c5-13911dc7a623;mod=0;room-id=61927669;subscriber=0;tmi-sent-ts=1525115130064;turbo=0;user-id=61927669;user-type= :timeshifter08!timeshifter08@timeshifter08.tmi.twitch.tv PRIVMSG
  #chatrooms:61927669:a0c1e70d-7d52-491a-bcd5-6db6a5492d64 :alooooha
-
-
                 */
 
-                //var parts = line.split(' ');
                 var userdata = parts[0].split(';');
 
                 var userdata_obj = {};
@@ -165,52 +147,40 @@ function TwitchClient(opts) {
 
                 var channel = line.split(' ')[3].substring(1).trim().toLowerCase();
 
-                //channel record has already been created, bail
-                //if (_channelIds[channel] != undefined) {
-                //    return;
-                //}
-
-                //for (var i = 0; i < userdata.length; i++) {
-                //    if (userdata[i].indexOf('room-id') == 0) {
-                //        _channelIds[channel] = userdata[i].split('=')[1];
-                //        _channelNames[userdata[i].split('=')[1]] = channel;
-                //        _channelRooms[userdata[i].split('=')[1]] = {};
-                //        break;
-                //    }
-                //}
-
                 if (client.onRoomstate)
                     client.onRoomstate(channel, userdata_obj);
 
             }
             else if (parts[2] == 'USERNOTICE') {
-
                 client.onUsernotice(line);
-
                 var msgParts = parts[0].split(';');
-
+                var isSub = 0;
                 for (var i = 0; i < msgParts.length; i++) {
-
                     if (msgParts[i] == 'msg-id=sub') {
-                        isSub = 0;
-                        break;
-                    }
-                    else if (msgParts[i] == 'msg-id=resub') {
                         isSub = 1;
                         break;
-                    }
-                    else if (msgParts[i] == 'msg-id=subgift') {
+                    } else if (msgParts[i] == 'msg-id=resub') {
                         isSub = 2;
                         break;
+                    } else if (msgParts[i] == 'msg-id=subgift') {
+                        isSub = 3;
+                        break;
                     }
-
                 }
-
-
-
-            }
-            else {
-                //console.log('>' + line);
+                switch (isSub) {
+                    case 1: /* first sub */
+                        if (client.onSub)
+                            client.onSub(line);
+                        break;
+                    case 2: /* resub */
+                        if (client.onReSub)
+                            client.onReSub(line);
+                        break;
+                    case 3: /* sub gift */
+                        if (client.onSubGift)
+                            client.onSubGift(line);
+                        break;
+                }
             }
         }
     }
@@ -229,12 +199,6 @@ function TwitchClient(opts) {
             client.Connect();
         }, 1000);
     }
-
-
-
-
-
-
 
     //public functions
 
@@ -279,9 +243,7 @@ function TwitchClient(opts) {
             //}
             //else if (c.split(':').length == 2) {
 
-
             //}
-
 
             if (c[0] != '#')
                 c = '#' + c;
@@ -328,7 +290,6 @@ function TwitchClient(opts) {
         }
     }
 
-
     //events
 
     function SetDefaultEventHandled(evt) {
@@ -362,11 +323,8 @@ function TwitchClient(opts) {
         SetDefaultEventHandled('onUsernotice');
     }
 
-
-
     ///gooooo!
     this.Connect();
-
 
     this.DebugFunction = function () {
         console.log('channel id list:   ', _channelIds);
